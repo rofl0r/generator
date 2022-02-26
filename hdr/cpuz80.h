493278eaea567174a3e9d21d1b7531b675d590a3
rofl0r <rofl0r@users.noreply.github.com>
2022-02-26 10:28:31 +0000

    memz80: improve z80 memory access speed using inline func

--- hdr/cpuz80.h
+++ hdr/cpuz80.h
@@ -1,6 +1,7 @@
 #ifndef CPUZ80_HEADER_FILE
 #define CPUZ80_HEADER_FILE
 
+#define Z80_EXPORT static
 #include "z80.h"
 typedef struct z80 CONTEXTMZ80;
 extern CONTEXTMZ80 cpuz80_z80; /* extern'd for save state code */
